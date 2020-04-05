e250f091e1ec3b40a0200ebf7774c98d395d2bed
CodexVic <ovajayi@gmail.com>
2020-04-05 03:05:12 +0100

    clearCart, removeItems and increment at context.js

--- src/context.js
+++ src/context.js
@@ -72,19 +72,52 @@ closeModal = () =>{
 }
 
 increment =(id) =>{
-    console.log('This is increment method')
+    let tempCart = [...this.state.cart]
+    const selectedProduct =tempCart.find(item =>item.id ===id)
+    const index = tempCart.indexOf(selectedProduct)
+    const product = tempCart[index]
+
+    product.count = product.count + 1
+    product.total = product.count *product.price
+    
+
+    this.setState(()=>{
+        return{cart:[...tempCart]}
+    },()=>{
+        this.addTotals()
+    })
 }
  
 decrement =(id) =>{
     console.log('This is decrement method')
 }
 
-removeItem =(id) =>{
-    console.log('item removed ')
+removeItem =id =>{
+    let tempProducts = [...this.state.products]
+    let tempCart =[...this.state.cart]
+
+    tempCart = tempCart.filter(item => item.id !==id)
+    const index =tempProducts.indexOf(this.getItem(id))
+    let removedProduct = tempProducts[index]
+    removedProduct.inCart = false
+    removedProduct.count =0
+    removedProduct.total = 0
+
+    this.setState (()=>{
+        return {
+            cart:[...tempCart],
+            product:[...tempProducts]
+        }
+    }, ()=>{
+        this.addTotals()
+    })
 }
 clearCart =() =>{
     this.setState(()=>{
         return {cart:[]}
+    },()=>{
+        this.setProducts()
+        this.addTotals()
     })
 }
 
