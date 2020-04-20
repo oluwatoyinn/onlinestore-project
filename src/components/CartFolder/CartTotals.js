5b0b3ab2dd3014b42b169eb7fc7a075c14347574
CodexVic <ovajayi@gmail.com>
2020-04-20 23:50:36 +0100

    modification of the product page navbar

--- src/components/CartFolder/CartTotals.js
+++ src/components/CartFolder/CartTotals.js
@@ -12,13 +12,13 @@ export default function CartTotals({value}) {
                             <button className="btn btn-outline-danger text-uppercase mb-3 px-5" onClick={()=>clearCart()}>clear cart</button>
                         </Link>
                         <h5>
-                            <span className="text-title">subtotal : <strong>$ {cartSubTotal} </strong> </span>
+                            <span className="text-title">subtotal : <strong># {cartSubTotal} </strong> </span>
                         </h5>
                         <h5>
-                            <span className="text-title">tax : <strong>$ {cartTax} </strong> </span>
+                            <span className="text-title">tax : <strong># {cartTax} </strong> </span>
                         </h5>
                         <h5>
-                            <span className="text-title">total : <strong>$ {cartTotal} </strong> </span>
+                            <span className="text-title">total : <strong># {cartTotal} </strong> </span>
                         </h5>
                     </div>
                 </div>
